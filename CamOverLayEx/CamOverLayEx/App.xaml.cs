de789314958dc14dca1da6b116c3a670533e3665
S Ravi Kumar <xltants@xltants.com>
2018-03-10 13:58:48 +0530

    Add project files.

---
+++ CamOverLayEx/CamOverLayEx/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CamOverLayEx
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new CamOverLayEx.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ CamOverLayEx/CamOverLayEx/MainPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Plugin.Media;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace CamOverLayEx
+{
+    public partial class MainPage : ContentPage
+	{
+
+#if XAMARINIOS
+Func<object> SetOverlay = () =>
+{
+    var imageView = new UIImageView(UIImage.FromBundle("camera-overlay.png"));
+    imageView.ContentMode = UIViewContentMode.ScaleAspectFit;
+
+    var screen = UIScreen.MainScreen.Bounds;
+    imageView.Frame = screen;
+
+    return imageView;
+};
+#endif
+        public MainPage()
+		{
+            InitializeComponent();
+            takePhoto.Clicked += async (sender, args) =>
+            {
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                    return;
+                }
+                try
+                {
+
+#if XAMARINIOS
+                    var vOverLay = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                    {
+                        OverlayViewProvider = func,
+                        Directory = "Sample",
+                        Name = "test.jpg"                        
+                    };
+                    file = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(vOverLay);
+#else
+                    file = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                    {
+                        Directory = "Sample",
+                        Name = "test.jpg",
+                        SaveToAlbum = saveToGallery.IsToggled
+
+                    });
+#endif
+                    if (file == null)
+                        return;
+
+                    await DisplayAlert("File Location", (saveToGallery.IsToggled ? file.AlbumPath : file.Path), "OK");
+
+                    image.Source = ImageSource.FromStream(() =>
+                    {
+                        var stream = file.GetStream();
+                        file.Dispose();
+                        return stream;
+                    });
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Uh oh", "Something went wrong, but don't worry we captured it in Xamarin Insights! Thanks.", "OK");
+                }
+            };
+
+            pickPhoto.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                    return;
+                }
+                try
+                {
+                    Stream stream = null;
+                    var file = await CrossMedia.Current.PickPhotoAsync().ConfigureAwait(true);
+
+
+                    if (file == null)
+                        return;
+
+                    stream = file.GetStream();
+                    file.Dispose();
+
+                    image.Source = ImageSource.FromStream(() => stream);
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Uh oh", "Something went wrong, but don't worry we captured it in Xamarin Insights! Thanks.", "OK");
+                }
+            };
+
+            takeVideo.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                    return;
+                }
+
+                try
+                {
+                    var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+                    {
+                        Name = "video.mp4",
+                        Directory = "DefaultVideos",
+                        SaveToAlbum = saveToGallery.IsToggled
+                    });
+
+                    if (file == null)
+                        return;
+
+                    await DisplayAlert("Video Recorded", "Location: " + (saveToGallery.IsToggled ? file.AlbumPath : file.Path), "OK");
+
+                    file.Dispose();
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Uh oh", "Something went wrong, but don't worry we captured it in Xamarin Insights! Thanks.", "OK");
+                }
+            };
+
+            pickVideo.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsPickVideoSupported)
+                {
+                    await DisplayAlert("Videos Not Supported", ":( Permission not granted to videos.", "OK");
+                    return;
+                }
+                try
+                {
+                    var file = await CrossMedia.Current.PickVideoAsync();
+
+                    if (file == null)
+                        return;
+
+                    await DisplayAlert("Video Selected", "Location: " + file.Path, "OK");
+                    file.Dispose();
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Uh oh", "Something went wrong, but don't worry we captured it in Xamarin Insights! Thanks.", "OK");
+                }
+            };
+        }
+    }
+}
+
